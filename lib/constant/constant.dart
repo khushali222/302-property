088946b5cbaba2c4a1afee248e757e0e8d060f9e
Khushali SARVAIYA <khushisarvaiya2@gmail.com>
2024-10-14 18:31:14 +0530

    popup14/10/2024

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -7,7 +7,7 @@ String image_url = "https://saas.cloudrentalmanager.com/api/images/get-file/";
 //String image_url = "http://192.168.182.128:4000/api/images/get-file/";
 
 //String Api_url = "http://192.168.39.1:4000";
-String Api_url = "http://192.168.1.19:4000";
+String Api_url = "http://192.168.1.20:4000";
 
 
 //String Api_url = "https://saas.cloudrentalmanager.com";
