c78416e6720c23d735e66167d2d13bebbd29133c
Khushali SARVAIYA <khushisarvaiya2@gmail.com>
2024-11-20 14:11:04 +0530

    past rent due

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -11,10 +11,10 @@ String image_url = "https://saas.cloudrentalmanager.com/api/images/get-file/";
 
 //String Api_url = "http://192.168.1.14:4000";
 
-String Api_url = "http://192.168.1.14:4000";
+//String Api_url = "http://192.168.1.33:4000";
 
 
-//String Api_url = "https://saas.cloudrentalmanager.com";
+String Api_url = "https://saas.cloudrentalmanager.com";
 
 String image_upload_url = "https://saas.cloudrentalmanager.com";
 
